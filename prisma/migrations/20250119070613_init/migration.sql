45da014a79609d07596af897138f3332a6bc77b3
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2025-01-21 12:40:12 +0530

    feat: db setup

---
+++ prisma/migrations/20250119070613_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "HtmlEntity" (
+    "id" TEXT NOT NULL,
+    "glyph" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "unicode" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+
+    CONSTRAINT "HtmlEntity_pkey" PRIMARY KEY ("id")
+);
